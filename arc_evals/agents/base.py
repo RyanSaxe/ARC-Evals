23f2d603a7c862725fbff08f7444b2e59e1d9f88
RyanSaxe <ryancsaxe+1@gmail.com>
2024-03-24 14:24:42 +0530

    claude agent setup

--- arc_evals/agents/base.py
+++ arc_evals/agents/base.py
@@ -8,17 +8,21 @@ import numpy as np
 
 
 class Agent(abc.ABC):
-    def __init__(self, data_path: Path | str, eval_path: Path | str, name: str):
+    def __init__(self, data_path: Path | str, eval_path: Path | str, name: str, overwrite: bool = False):
         self.data_path = Path(data_path)
         self.name = name
-        i = 1
-        while self.name in os.listdir(eval_path):
-            i += 1
-            self.name = f"{name}_{i}"
-        self.eval_path = Path(eval_path) / self.name
-        os.mkdir(self.eval_path)
+        if overwrite:
+            self.eval_path = Path(eval_path) / self.name
+        else:
+            i = 1
+            while self.name in os.listdir(eval_path):
+                i += 1
+                self.name = f"{name}_{i}"
+            self.eval_path = Path(eval_path) / self.name
+        self.eval_path.mkdir(parents=True, exist_ok=True)
 
     def __call__(self, json_filename: str) -> None:
+        predictions = None
         if not json_filename.endswith(".json"):
             json_filename = f"{json_filename}.json"
         output_metrics = {"status": "success"}
@@ -29,11 +33,14 @@ class Agent(abc.ABC):
             metrics = [
                 self.evaluate(answer["output"], prediction) for answer, prediction in zip(data["test"], predictions)
             ]
-            metrics = {k: np.average(list(map(itemgetter(k), metrics))) for k in metrics[0].keys()}
+            output_metrics["metrics"] = [{k: np.average(v) for k, v in metric.items()} for metric in metrics]
+            output_metrics["predictions"] = predictions
         except Exception as e:
             # log e I guess . . . maybe even in this file?
-            metrics = {"status": "failure", "traceback": str(e)}
-        output_metrics |= metrics
+            output_metrics["status"] = "failure"
+            output_metrics["traceback"] = str(e)
+            # if the error happens during metric calculation, we still want access to prediction
+            output_metrics["predictions"] = predictions
         with open(self.eval_path / json_filename, "w") as f:
             json.dump(output_metrics, f)
 
@@ -41,5 +48,12 @@ class Agent(abc.ABC):
     def call(self, data: dict) -> list[list[list[int]]]: ...
 
     def evaluate(self, answer: list[list[int]], prediction: list[list[int]]) -> dict[str, float]:
-        answer, prediction = np.asarray(answer), np.asarray(prediction)
-        return {"cell_accuracy": 1 - np.average(answer == prediction)}
+        metrics = dict()
+        answer_np, prediction_np = np.asarray(answer), np.asarray(prediction)
+        metrics["cell_accuracy_all"] = np.average(answer_np == prediction_np)
+        for i in range(10):
+            idx = np.where(answer_np == i)
+            indexed_answer = answer_np[idx]
+            if indexed_answer.size > 0:
+                metrics[f"cell_accuracy_{i}"] = np.average(answer_np[idx] == prediction_np[idx])
+        return metrics
--- arc_evals/agents/test_agent.py
+++ arc_evals/agents/test_agent.py
@@ -8,13 +8,13 @@ class TestAgent(Agent):
     """An Agent that is allowed to cheat, so all metrics should be perfect"""
 
     def call(self, data):
-        return [answer for answer in data["test"]]
+        return [answer["output"] for answer in data["test"]]
 
 
 EVAL_PATH = Path(__file__).parent.parent.parent / "evals"
 DATA_PATH = Path(__file__).parent.parent.parent / "data/training"
 
 if __name__ == "__main__":
-    agent = TestAgent(DATA_PATH, EVAL_PATH, "cheater")
-    for json_filename in os.listdir(DATA_PATH):
+    agent = TestAgent(DATA_PATH, EVAL_PATH, "cheater", overwrite=True)
+    for i, json_filename in enumerate(os.listdir(DATA_PATH)):
         agent(json_filename)
